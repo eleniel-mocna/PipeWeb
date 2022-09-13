661dd261a915f25409d5887c1f15d2bd49edc8e7
Dominika Šenová <domisenova@seznam.cz>
2022-09-13 23:28:06 +0200

    Fix test_script

--- defaultUser/scripts/test_script.sh
+++ defaultUser/scripts/test_script.sh
@@ -1,6 +1,6 @@
-printf "Test script started...\n">"test_output"
+echo "Test script started...">"test_output"
 for (( i = 1; i <= $#; i++ )); do
     sleep 5
     echo "$i: ${@:i:1}">>"test_output"
 done
-printf "Test script ended!\n">>"test_output"
+echo "Test script ended!">>"test_output"
